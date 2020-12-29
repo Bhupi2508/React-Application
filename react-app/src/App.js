0be67baf0ab293c62031af0746bd81e657415d17
Bhupendra Singh <bhupendrasingh.ec18@gmail.com>
2020-12-29 21:08:59 +0530

    Create login and signup component and modify with router

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/login/login.component";
+import SignUp from "./components/signUp/signup.component";
 
 function App() {
-  return 
-  // (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
+  return (<Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/login"}>DashBoard</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/login"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/register"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="outer">
+        <div className="inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
+  );
 }
 
 export default App;
